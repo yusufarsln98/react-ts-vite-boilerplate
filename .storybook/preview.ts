9c3e6ef92664a4c9d67d2bc05d25c396fd2e9612
Yusuf Arslan <yusufarsln98@outlook.com>
2025-02-25 15:49:55 +0300

    feat: theme decorator added to storybook

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -1,4 +1,5 @@
 import type { Preview } from '@storybook/react'
+import { withThemeByClassName } from '@storybook/addon-themes'
 
 const preview: Preview = {
 	parameters: {
@@ -9,6 +10,19 @@ const preview: Preview = {
 			},
 		},
 	},
+
+		decorators: [
+		// Adds theme switching support.
+		// NOTE: requires setting "darkMode" to "class" in your tailwind config
+		withThemeByClassName({
+			themes: {
+				light: 'light',
+				dark: 'dark',
+			},
+			defaultTheme: 'light',
+		}),
+	],
 }
 
 export default preview
+
