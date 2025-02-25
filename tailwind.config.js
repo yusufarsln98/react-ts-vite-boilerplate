11d9008dc3af65b0cd3d8fb19f4390c4153fdcd6
Yusuf Arslan <yusufarsln98@outlook.com>
2025-02-25 11:20:21 +0300

    feat: storybook added to the project

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./index.html', './src/**/*.{ts,tsx,js,jsx}'],
+  content: ['./index.html', './src/**/*.{ts,tsx,js,jsx. }'],
   theme: {
     extend: {},
   },
