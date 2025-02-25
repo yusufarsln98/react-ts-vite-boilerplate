6aca44b1565dc7801e61117a297e21e0d1693fc1
Yusuf Arslan <yusufarsln98@outlook.com>
2025-02-25 13:52:21 +0300

    feat: prettier and eslint configs

--- eslint.config.js
+++ eslint.config.js
@@ -3,26 +3,34 @@ import globals from 'globals'
 import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
 import tseslint from 'typescript-eslint'
+import prettier from 'eslint-config-prettier'
+import prettierPlugin from 'eslint-plugin-prettier'
 
 export default tseslint.config(
-  { ignores: ['dist'] },
-  {
-    extends: [js.configs.recommended, ...tseslint.configs.recommended],
-    files: ['**/*.{ts,tsx}'],
-    languageOptions: {
-      ecmaVersion: 2020,
-      globals: globals.browser,
-    },
-    plugins: {
-      'react-hooks': reactHooks,
-      'react-refresh': reactRefresh,
-    },
-    rules: {
-      ...reactHooks.configs.recommended.rules,
-      'react-refresh/only-export-components': [
-        'warn',
-        { allowConstantExport: true },
-      ],
-    },
-  },
+	{ ignores: ['dist'] },
+	{
+		extends: [
+			js.configs.recommended,
+			...tseslint.configs.recommended,
+			prettier, // Disable ESLint rules that conflict with Prettier
+		],
+		files: ['**/*.{ts,tsx}'],
+		languageOptions: {
+			ecmaVersion: 2020,
+			globals: globals.browser,
+		},
+		plugins: {
+			'react-hooks': reactHooks,
+			'react-refresh': reactRefresh,
+			prettier: prettierPlugin, // Add Prettier as a plugin
+		},
+		rules: {
+			...reactHooks.configs.recommended.rules,
+			'react-refresh/only-export-components': [
+				'warn',
+				{ allowConstantExport: true },
+			],
+			'prettier/prettier': 'warn', // Enable Prettier formatting warnings
+		},
+	}
 )
---
+++ prettier.config.js
@@ -0,0 +1,22 @@
+const prettierConfig = {
+	printWidth: 80,
+	tabWidth: 2,
+	useTabs: true,
+	semi: false,
+	singleQuote: true,
+	quoteProps: 'as-needed',
+	jsxSingleQuote: true,
+	trailingComma: 'es5',
+	bracketSpacing: true,
+	bracketSameLine: false,
+	arrowParens: 'always',
+	requirePragma: false,
+	insertPragma: false,
+	proseWrap: 'preserve',
+	htmlWhitespaceSensitivity: 'css',
+	endOfLine: 'lf',
+	embeddedLanguageFormatting: 'auto',
+	singleAttributePerLine: true,
+}
+
+export default prettierConfig
