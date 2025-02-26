3bfa76571bd4ccd47d954f5a4d2308a89ee7c785
Yusuf Arslan <yusufarsln98@outlook.com>
2025-02-26 10:05:26 +0300

    fix: normalizing local translation path

--- vite.config.ts
+++ vite.config.ts
@@ -2,13 +2,22 @@ import tailwindcss from '@tailwindcss/vite'
 import { TanStackRouterVite } from '@tanstack/router-plugin/vite'
 import react from '@vitejs/plugin-react-swc'
 import path from 'node:path'
-import { defineConfig } from 'vite'
+import { defineConfig, normalizePath } from 'vite'
+import { viteStaticCopy } from 'vite-plugin-static-copy'
 
 export default defineConfig({
 	plugins: [
 		react(),
 		tailwindcss(),
 		TanStackRouterVite({ target: 'react', autoCodeSplitting: true }),
+		viteStaticCopy({
+			targets: [
+				{
+					src: normalizePath(path.resolve('./src/assets/locales')),
+					dest: normalizePath(path.resolve('./dist')),
+				},
+			],
+		}),
 	],
 	resolve: {
 		alias: {
